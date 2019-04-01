034f3f4d98a18bb1bece2edc8c1015eacd1d5639
Stuart Rendall <14021635@uhi.ac.uk>
2019-04-01 22:20:41 +0100

    console errors sorted

--- src/ListQuizzes.js
+++ src/ListQuizzes.js
@@ -16,18 +16,19 @@ class ListQuizzes extends Component {
     }
 
     return (
-      <div class='row'>
+      <div className="row">
 
       {quizvalues.map((quiz)=>(
-        <div class="col-sm-6 col-md-4">
-					<div class="card m-2 text-center attempted">
-						<div class="card-body">
-							<h5 class="card-title ">{quiz.name}</h5>
-							<div class="row mt-3 mb-3 justify-content-center">
-								<div class="col-auto card-text">completed:</div>
-								<div class="col-auto card-text">{quiz.answeredCorrectly.length}/{quiz.questions.length} correct</div>
-							</div>
-							<a href="./complete.html" class="btn">Try again?</a>
+        <div key={quiz.id} className="col-sm-6 col-md-4">
+					<div className="card m-2 text-center attempted">
+						<div className="card-body">
+							<h5 className="card-title ">{quiz.name}</h5>
+
+								<div className="card-text">started:</div>
+								<div className="card-text">{quiz.answeredCorrectly.length}/{quiz.answeredCorrectly.length+quiz.answeredIncorrectly.length} correct so far</div>
+                <div className="card-text mb-3">{quiz.answeredCorrectly.length+quiz.answeredIncorrectly.length}/{quiz.questions.length} questions attempted</div>
+
+							<a href="./complete.html" className="btn">Resume?</a>
 						</div>
 					</div>
 				</div>
