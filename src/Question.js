90f353f7c9f7499b41869b7a6b0f851e50cf6222
Stuart Rendall <14021635@uhi.ac.uk>
2019-04-03 23:28:16 +0100

    results page added

--- src/Question.js
+++ src/Question.js
@@ -16,42 +16,59 @@ class Question extends Component {
     const { quizId, quiz, quizName, question, questionText } = this.props
 
     console.log('question.js',this)
-    console.log('\nquestion.id', question.id)
+    //console.log('\nquestion.id', question.id)
     // console.log('\nquestionText', questionText)
     // console.log('\nquestionAttempted', question.questionAttempted)
 
-    return (
-      <div className="container-fluid">
-			<div className="row mt-3 mb-3">
-				<div className="col-sm">
-					<h3 className="text-center">{quizName}</h3>
-				</div>
-        <div className="col-sm">
-					<h3 className="text-center">Question {quiz.questions.length-quiz.questionsLeft.length +1}
-          /{quiz.questions.length}</h3>
-				</div>
-			</div>
-			<div className="row">
-				<div className="col-sm">
-					<div className="p-3 mb-2 bg-light">
-							<p>{question.questionText}</p>
-						<div className="text-center">
-							<img className="mb-4 rounded img-fluid" src="http://i-want-to-study-engineering.org/figs/circle_intersection.png"/>
-						</div>
-					</div>
-				</div>
+    if (question) {
+      return (
+        <div className="container-fluid">
+    			<div className="row mt-3 mb-3">
+    				<div className="col-sm">
+    					<h3 className="text-center">{quizName}</h3>
+    				</div>
+            <div className="col-sm">
+    					<h3 className="text-center">Question {quiz.questions.length-quiz.questionsLeft.length +1}
+              /{quiz.questions.length}</h3>
+    				</div>
+    			</div>
+    			<div className="row">
+    				<div className="col-sm">
+    					<div className="p-3 mb-2 bg-light">
+    							<p>{question.questionText}</p>
+    						<div className="text-center">
+    							<img className="mb-4 rounded img-fluid" src="http://i-want-to-study-engineering.org/figs/circle_intersection.png"/>
+    						</div>
+    					</div>
+    				</div>
 
-				<div className="col-sm">
-					<div className="p-3 mb-2 bg-light">
-          <ListAnswerButtons
-            answers = {question.answers}
-            answerSelected = {this.answeredQuestion}
-          />
-				</div>
-			</div>
-		</div>
-    </div>
+    				<div className="col-sm">
+    					<div className="p-3 mb-2 bg-light">
+              <ListAnswerButtons
+                answers = {question.answers}
+                answerSelected = {this.answeredQuestion}
+              />
+    				</div>
+    			</div>
+    		</div>
+      </div>
     );
+  } else {
+    return(
+      <div className="container-fluid">
+      <div className="col-sm">
+        <h3 className="text-center">{quizName}</h3>
+      </div>
+        <div className="row mt-3 mb-3">
+          <h1>Quiz Finished</h1>
+        </div>
+        <div className="row mt-3 mb-3">
+          <h3>You scored {quiz.answeredCorrectly.length} out of {quiz.questions.length}</h3>
+        </div>
+      </div>
+    )
+
+  }
   }
 }
 
